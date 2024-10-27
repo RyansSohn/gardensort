365a77c35f9350cdef15c302680dc144c11787d8
RyansSohn <acc1@f-schwichow.de>
2024-10-27 22:05:31 +0100

    Quarkus Install + Basic Java Setup

---
+++ gardensort-quarkus/quarkus-gardensort/src/main/java/gardensort/Main.java
@@ -0,0 +1,5 @@
+package Git Clones.gardensort.gardensort-quarkus.quarkus-gardensort.src.main.java.gardensort;
+
+public class Main {
+
+}
---
+++ gardensort-quarkus/quarkus-gardensort/src/test/java/gardensort/GardenSortResourceIT.java
@@ -0,0 +1,8 @@
+package gardensort;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GardenSortResourceIT extends GardenSortResourceTest {
+    // Execute the same tests but in packaged mode.
+}
